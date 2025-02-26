e9f3ce45b7948a26aea01aab8a8daee24f5cc1e6
Sakai-san <o_edo@hotmail.com>
2025-02-26 05:42:19 +0100

    event icon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import { getRooms } from "../api";
 import List from "@mui/material/List";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import DoNotDisturbAltIcon from "@mui/icons-material/DoNotDisturbAlt";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import EventBusyIcon from "@mui/icons-material/EventBusy";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Divider from "@mui/material/Divider";
 import { ButtonLink } from "../components/ButtonLink";
@@ -42,7 +42,7 @@ function Index() {
             </ListItemAvatar>
             <ListItemText primary={room.name} />
             <ListItemIcon>
-              {room.busy ? <DoNotDisturbAltIcon color="error" /> : <CheckCircleOutlineIcon color="success" />}
+              {room.busy ? <EventBusyIcon color="error" /> : <EventAvailableIcon color="success" />}
             </ListItemIcon>
           </ButtonLink>
           {index !== rooms.length - 1 && <Divider variant="inset" component="li" />}
