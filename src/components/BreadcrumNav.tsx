790536f624bca1cf38f9125c8e774c97f274d37f
Sakai-san <o_edo@hotmail.com>
2025-03-02 20:13:35 +0100

    remove app file

--- src/components/BreadcrumNav.tsx
+++ src/components/BreadcrumNav.tsx
@@ -6,31 +6,20 @@ export const BreadcrumbNav = () => {
   const deviceMatch = useMatch({ from: "/rooms/$roomId/devices/$deviceId", shouldThrow: false });
 
   const items = deviceMatch
-    ? [{ label: "Room", href: `/rooms/${deviceMatch.params.roomId}` }].concat(
-        [deviceMatch].map(({ loaderData }) => {
-          return {
-            label: loaderData?.name,
-          };
-        })
-      )
+    ? [
+        { label: "Home", href: "/" },
+        { label: "Room", href: `/rooms/${deviceMatch.params.roomId}` },
+        { label: deviceMatch.loaderData?.name },
+      ]
     : roomMatch
-      ? [roomMatch].map(({ loaderData }) => ({
-          label: loaderData?.name,
-        }))
+      ? [{ label: "Home", href: "/" }, { label: roomMatch.loaderData?.name }]
       : [{ label: "Home", href: "/" }];
 
   return (
     <div>
       <Box component="ul" sx={{ listStyleType: "none", display: "flex", gap: 5 }}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
         {items.map((item, index) => (
-          <li key={index}>
-            {item?.href ? <Link to={item.href}>{item.label}</Link> : <span>{item.label}</span>}
-
-            {/*index < items.length - 1 && <span>{">"}</span> */}
-          </li>
+          <li key={index}>{item?.href ? <Link to={item.href}>{item.label}</Link> : <span>{item.label}</span>}</li>
         ))}
       </Box>
     </div>
