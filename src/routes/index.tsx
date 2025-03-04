0fa85df8df876ead270b4ca4624bccb825ff2478
Sakai-san <o_edo@hotmail.com>
2025-03-04 23:18:46 +0100

    new dependency

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,7 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+import { IconButton } from "@mui/material";
 
 export const Route = createFileRoute("/")({
   loader: getRooms,
@@ -52,9 +53,11 @@ function Index() {
             <PopupState variant="popover" popupId="demo-popup-menu">
               {(popupState) => (
                 <Fragment>
-                  <Button variant="contained" {...bindTrigger(popupState)}>
-                    Dashboard
-                  </Button>
+                  <IconButton>
+                    <Button variant="contained" {...bindTrigger(popupState)}>
+                      Dashboard
+                    </Button>
+                  </IconButton>
                   <Menu {...bindMenu(popupState)}>
                     <MenuItem onClick={popupState.close}>Profile</MenuItem>
                     <MenuItem onClick={popupState.close}>My account</MenuItem>
