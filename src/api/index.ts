7b1b0596407bd8f437ccae39411cb434ceb4b457
Sakai-san <o_edo@hotmail.com>
2025-03-08 00:12:13 +0100

    api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,4 +2,3 @@ export * from "./rooms";
 export { getDevice } from "./getDevice";
 export { postRelease } from "./postRelease";
 export { postBook } from "./postBook";
-export { patchRoom } from "./patchRoom";
