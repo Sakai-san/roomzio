4f18921023dd475dbb1d7089f10ffcb9a03dc8f2
Sakai-san <o_edo@hotmail.com>
2025-03-14 09:13:47 +0100

    routing

--- src/Provider/RoomOperation.ts
+++ src/Provider/RoomOperation.ts
@@ -35,32 +35,33 @@ function RoomOperationsProvider({ children }: PropsWithChildren) {
   };
 
   return (
+
     <>
-      <RoomOperationsContext.Provider value={providerValue}>{children}</RoomOperationsContext.Provider>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
+    <RoomOperationsContext.Provider value={providerValue}>{children}</RoomOperationsContext.Provider>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {"Use Google's location service?"}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Let Google help apps determine location. This means sending anonymous
+          location data to Google, even when no apps are running.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+        <Button onClick={handleClose} autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </>
+  )
 }
 
 const useRoomOperations = () => {
