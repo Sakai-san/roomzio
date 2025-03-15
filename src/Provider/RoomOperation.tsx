7cb0446b219fc34e1de4b562dbd4c9bd13363658
Sakai-san <o_edo@hotmail.com>
2025-03-15 01:07:21 +0100

    context

--- src/Provider/RoomOperation.tsx
+++ src/Provider/RoomOperation.tsx
@@ -1,15 +1,28 @@
 import { Fragment, createContext, PropsWithChildren, useContext, useState } from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
 import { RoomType } from "../api";
 
 interface RoomOperations {
   deleteRoom: (room: RoomType) => Promise<void>;
   renameRoom: (room: RoomType) => Promise<void>;
 }
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
 
 const RoomOperationsContext = createContext<RoomOperations>({} as RoomOperations);
 
@@ -36,26 +49,42 @@ function RoomOperationsProvider({ children }: PropsWithChildren) {
   return (
     <Fragment>
       <RoomOperationsContext.Provider value={providerValue}>{children}</RoomOperationsContext.Provider>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to Google, even when no
-            apps are running.
-          </DialogContentText>
+      <BootstrapDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={!!pendingRoom}>
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Modal title
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget
+            quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet
+            rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl
+            consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+          </Typography>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
+          <Button autoFocus onClick={handleClose}>
+            Save changes
           </Button>
         </DialogActions>
-      </Dialog>
+      </BootstrapDialog>
     </Fragment>
   );
 }
